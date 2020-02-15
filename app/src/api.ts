44f6de7fa5a2c9d52f280d6d2af204e7e874786b
Simon Knott <simoknott@gmail.com>
2020-02-15 16:31:23 +0100

    Add function to refresh access token

--- app/src/api.ts
+++ app/src/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios"
 
+const BASE_URL = "https://whitelabel-app-api-bvg.trafi.com";
+
+interface TokenPair {
+    refreshToken: string;
+    accessToken: string;
+}
+
 export async function fetchHistory(token: string) {
-    const response = await axios.get("https://whitelabel-app-api-bvg.trafi.com/v1/history", {
+    const response = await axios.get(BASE_URL + "/v1/history", {
         headers: {
             Authorization: "Bearer " + token
         }
@@ -10,12 +17,24 @@ export async function fetchHistory(token: string) {
     return json
 }
 
-export async function signIn(email: string, password: string) {
-    const response = await axios.post("https://whitelabel-app-api-bvg.trafi.com/v1/users/signin", {
+/**
+ * @returns a new token pair
+ */
+export async function refresh(refreshToken: string): Promise<TokenPair> {
+    const response = await axios.post(
+        BASE_URL + "/v1/users/refreshtoken",
+        { refreshToken }
+    );
+
+    return response.data;
+}
+
+export async function signIn(email: string, password: string): Promise<TokenPair> {
+    const response = await axios.post(BASE_URL + "/v1/users/signin", {
         acceptedTermKeys: ["terms", "privacy"],
         email,
         password
     })
     const json = response.data
-    return json.authData.accessToken
+    return json.authData;
 }
