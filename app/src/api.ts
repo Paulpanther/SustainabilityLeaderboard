36ef3a64a0bcdaf59a434ae1ca4a349524463f85
Simon Knott <simoknott@gmail.com>
2020-02-15 16:35:19 +0100

    Use axios instances

--- app/src/api.ts
+++ app/src/api.ts
@@ -1,6 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = "https://whitelabel-app-api-bvg.trafi.com";
+const trafi = axios.create({
+    baseUrl: "https://whitelabel-app-api-bvg.trafi.com",
+})
 
 interface TokenPair {
     refreshToken: string;
@@ -8,21 +10,24 @@ interface TokenPair {
 }
 
 export async function fetchHistory(token: string) {
-    const response = await axios.get(BASE_URL + "/v1/history", {
-        headers: {
-            Authorization: "Bearer " + token
+    const response = await trafi.get(
+        "/v1/history",
+        {
+            headers: {
+                Authorization: "Bearer " + token
+            }
         }
-    })
-    const json = response.data
-    return json
+    );
+
+    return response.data;
 }
 
 /**
  * @returns a new token pair
  */
 export async function refresh(refreshToken: string): Promise<TokenPair> {
-    const response = await axios.post(
-        BASE_URL + "/v1/users/refreshtoken",
+    const response = await trafi.post(
+        "/v1/users/refreshtoken",
         { refreshToken }
     );
 
@@ -30,11 +35,13 @@ export async function refresh(refreshToken: string): Promise<TokenPair> {
 }
 
 export async function signIn(email: string, password: string): Promise<TokenPair> {
-    const response = await axios.post(BASE_URL + "/v1/users/signin", {
-        acceptedTermKeys: ["terms", "privacy"],
-        email,
-        password
-    })
-    const json = response.data
-    return json.authData;
+    const response = await trafi.post(
+        "/v1/users/signin",
+        {
+            email,
+            password,
+            acceptedTermKeys: ["terms", "privacy"],
+        }
+    );
+    return response.data.authData;
 }
