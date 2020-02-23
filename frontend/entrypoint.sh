326f15ee7811e3bf3e357d9fe03886349aec1d01
Simon Knott <info@simonknott.de>
2020-02-23 14:36:17 +0100

    Remove extraneous quotes

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-envsubst '$SCOREBOARD_WS_HTTP_ADDRESS $SCOREBOARD_API_HTTP_ADDRESS $CORS_PROXY_ADDRESS' < /app/nginx.conf > /etc/nginx/conf.d/default.conf;
+envsubst '$SCOREBOARD_API_WS_ADDRESS $SCOREBOARD_API_HTTP_ADDRESS $CORS_PROXY_ADDRESS' < /app/nginx.conf > /etc/nginx/conf.d/default.conf;
 exec nginx -g 'daemon off;';
